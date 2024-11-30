cdc55d403ec1173a9a762a0753cd073b15b8e3fe
Real123678 <achan625@uwo.ca>
2024-11-30 16:28:06 -0500

    Fixed API annotations

--- backend/src/main/java/com/capstone/grocery/controller/GroceryController.java
+++ backend/src/main/java/com/capstone/grocery/controller/GroceryController.java
@@ -5,63 +5,82 @@ import com.capstone.grocery.model.GroceryList;
 import com.capstone.grocery.model.GroceryListItem;
 import com.capstone.grocery.model.User;
 import com.capstone.grocery.service.GroceryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
 public class GroceryController {
-    private GroceryService groceryService;
+    private final GroceryService groceryService;
+
+    @Autowired
+    public GroceryController(GroceryService groceryService) {
+        this.groceryService = groceryService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello World");
+    }
+
+    @PostMapping()
+    public ResponseEntity<String> postHello() {
+        return ResponseEntity.ok("Hello World");
+    }
 
     // get all the items in a list using the list id
     @GetMapping("/grocery-items-in-list/{groceryListId}")
-    List<GroceryItem> getGroceryItemsInList(@PathVariable Integer groceryListId) {
+    public List<GroceryItem> getGroceryItemsInList(@PathVariable Integer groceryListId) {
         List<GroceryListItem> groceryListItems = groceryService.findGroceryListItemsByListId(groceryListId);
         List<Integer> groceryItemsIds = groceryService.getItemIdsFromGroceryListItems(groceryListItems);
         return groceryService.findGroceryItemsByIds(groceryItemsIds);
     }
 
     @GetMapping("/grocery-list/{id}")
-    GroceryList getGroceryList(@PathVariable Integer id) {
+    public GroceryList getGroceryList(@PathVariable Integer id) {
         return groceryService.findGroceryListById(id);
     }
 
 //    @GetMapping("/grocery-list/{user_id}")
-//    GroceryList getGroceryListByUserId(@PathVariable Long user_id) {
+//    public GroceryList getGroceryListByUserId(@PathVariable Long user_id) {
 //        return;
 //    }
 
     @PostMapping("/grocery-item")
-    GroceryItem createGroceryItem(@RequestBody GroceryItem groceryItem) {
-        return groceryService.createGroceryItem(groceryItem);
+    public ResponseEntity<GroceryItem> createGroceryItem(@RequestBody GroceryItem groceryItem) {
+//        return groceryService.createGroceryItem(groceryItem);
+        return ResponseEntity.ok(groceryService.createGroceryItem(groceryItem));
     }
 
-    @GetMapping("/grocery-item/{id}")
-    GroceryItem getGroceryItemById(@PathVariable Integer id) {
+    @GetMapping("/grocery-list-item/{id}")
+    public GroceryItem getGroceryItemById(@PathVariable Integer id) {
         return groceryService.findGroceryItemById(id);
     }
 
     @GetMapping("/grocery-items/{name}")
-    List<GroceryItem> getGroceryItemByName(@PathVariable String name) {
+    public List<GroceryItem> getGroceryItemByName(@PathVariable String name) {
         return groceryService.findAllGroceryItemsByName(name);
     }
 
     @PostMapping("/grocery-list")
-    GroceryList createGroceryList(@RequestBody GroceryList groceryList) {
+    public GroceryList createGroceryList(@RequestBody GroceryList groceryList) {
         return groceryService.createGroceryList(groceryList);
     }
 
-    @GetMapping("/grocery-list/{id}")
-    GroceryList getGroceryListById(@PathVariable Integer id) {
-        return groceryService.findGroceryListById(id);
-    }
+//    @GetMapping("/grocery-list/{id}")
+//    public GroceryList getGroceryListById(@PathVariable Integer id) {
+//        return groceryService.findGroceryListById(id);
+//    }
 
     @PostMapping("/users")
-    User createUser(@RequestBody User user) {
+    public User createUser(@RequestBody User user) {
         return groceryService.createUser(user);
     }
 
     @GetMapping("/users/{id}")
-    User getUserById(@PathVariable Integer id) {
+    public User getUserById(@PathVariable Integer id) {
         return groceryService.getUserById(id);
     }
 
--- backend/src/main/java/com/capstone/grocery/repository/GroceryListRepository.java
+++ backend/src/main/java/com/capstone/grocery/repository/GroceryListRepository.java
@@ -1,10 +1,11 @@
 package com.capstone.grocery.repository;
 
 import com.capstone.grocery.model.GroceryList;
+import com.capstone.grocery.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GroceryListRepository extends JpaRepository<GroceryList, Integer> {
-    GroceryList findByUserId(Integer userId);
+    GroceryList findByUserId(User userId);
 }
