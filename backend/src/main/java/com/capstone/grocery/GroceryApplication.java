9f56522f3f5d27762e89235a62fdcbc4edaf6286
Real123678 <achan625@uwo.ca>
2024-11-30 13:58:56 -0500

    Pushed broken code to debug

--- backend/src/main/java/com/capstone/grocery/GroceryApplication.java
+++ backend/src/main/java/com/capstone/grocery/GroceryApplication.java
@@ -2,8 +2,10 @@ package com.capstone.grocery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.capstone.grocery.model")
 public class GroceryApplication {
 
 	public static void main(String[] args) {
