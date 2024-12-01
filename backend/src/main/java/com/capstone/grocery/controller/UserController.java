587c7dd3d1ea47f9f5f49f08bd6fecf2ebf25c94
Real123678 <achan625@uwo.ca>
2024-11-30 22:53:56 -0500

    Feat: backend login

--- backend/src/main/java/com/capstone/grocery/controller/UserController.java
+++ backend/src/main/java/com/capstone/grocery/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.capstone.grocery.controller;
 
+import com.capstone.grocery.dto.LoginCredentials;
+import com.capstone.grocery.exception.InvalidUserException;
 import com.capstone.grocery.model.User;
+import com.capstone.grocery.service.AuthenticationService;
 import com.capstone.grocery.service.GroceryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,10 +15,12 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final GroceryService groceryService;
+    private final AuthenticationService authenticationService;
 
     @Autowired
-    public UserController(GroceryService groceryService) {
+    public UserController(GroceryService groceryService, AuthenticationService authenticationService1) {
         this.groceryService = groceryService;
+        this.authenticationService = authenticationService1;
     }
 
     @PostMapping()
@@ -24,6 +31,16 @@ public class UserController {
     @GetMapping("/{id}")
     public User getUserById(@PathVariable Integer id) {
         return groceryService.getUserById(id);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody LoginCredentials loginCredentials) {
+        return ResponseEntity.ok(authenticationService.login(loginCredentials));
+    }
+
+    @ExceptionHandler(InvalidUserException.class)
+    public ResponseEntity<String> handleInvalidUserException(InvalidUserException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
     }
 
 }
---
+++ backend/src/main/java/com/capstone/grocery/dto/LoginCredentials.java
@@ -0,0 +1,9 @@
+package com.capstone.grocery.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginCredentials {
+    private String email;
+    private String password;
+}
