89022e9596429d563f3f3f41aca448ae2ddb5b1c
arushlow <arushlow@uwo.ca>
2024-11-30 21:57:23 -0500

    Created frontend form for users to change their password

--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -1,16 +1,42 @@
+import React, { useState } from 'react';
 import Navbar from '../components/Navbar';
 import '../style/Settings.css';
 
-type Settings = {
-
-};
+type Settings = {};
 
 const Settings: React.FC<Settings> = () => {
+  const [pwd, setPwd] = useState<string>("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const pwd = e.target.value;
+    setPwd(pwd);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('Password change:', pwd);
+    // Add logic to change password
+  };
   return (
     <>
       <Navbar />
-      <div className="weekly-deal">
-        <h2>{"Settings Page"}</h2>
+      <div className="settings-container">
+        <h2>Account Settings</h2>
+        <form className="settings-form" onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">Change Password</label>
+            <input
+              type="text"
+              id="password"
+              name="password"
+              value={pwd}
+              onChange={handleChange}
+              placeholder="Enter new password"
+              required
+            />
+          </div>
+          <button type="submit">Change Password</button>
+        </form>
       </div>
     </>
   );
