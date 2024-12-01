b6ac517644531a70769e96adf6273c18a23b055d
arushlow <arushlow@uwo.ca>
2024-11-30 19:41:59 -0500

    Setup template for login authentication token

--- frontend/src/components/UserSess.tsx
+++ frontend/src/components/UserSess.tsx
@@ -1,11 +1,30 @@
 // import {Link} from "react-scroll";
 // import { useLocation } from "react-router-dom/dist/umd/react-router-dom.development";
+import { jwtDecode } from 'jwt-decode';
+import React, { useEffect, useState } from 'react';
+
+type UserSessProps = {
+    user: string;
+    password: string;
+  };
+
+const UserSess: React.FC<UserSessProps> = ({user, password}) => {
+    const [token, setToken] = useState(() => localStorage.getItem('token') || null);
+    const [tokenExp, setTokenExp] = useState(() => token ? jwtDecode(token).exp : null)
+
+    useEffect(() => {
+        //code to get token 
 
 
-const UserSess: React.FC = () => {
+
+        //error if login failed
+
+
+        //set token
+        
+    });
     return (
-        <div className="navbar">
-        </div>
+        <></>
     );
 };
 
