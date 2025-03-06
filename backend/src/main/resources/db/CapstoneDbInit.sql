a7ba543e75ba2c33383e73bcb026a428f5f1654e
Real123678 <achan625@uwo.ca>
2025-03-06 23:05:51 +0000

    Split products and groceryItem in db

--- backend/src/main/resources/db/CapstoneDbInit.sql
+++ backend/src/main/resources/db/CapstoneDbInit.sql
@@ -1,8 +1,10 @@
-DROP TABLE grocery_list_items;
-DROP TABLE grocery_lists;
-DROP TABLE users;
-DROP TABLE grocery_items;
-DROP TABLE stores;
+DROP TABLE IF EXISTS grocery_list_items;
+DROP TABLE IF EXISTS grocery_lists;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS grocery_items;
+DROP TABLE IF EXISTS stores;
+DROP TABLE IF EXISTS blacklisted_tokens;
+DROP TABLE IF EXISTS products;
 
 CREATE TABLE stores (
     store_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -10,14 +12,21 @@ CREATE TABLE stores (
     address VARCHAR(255)
 );
 
+CREATE TABLE products (
+	product_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE grocery_items (
     item_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    product_id INT,
     price DECIMAL(10, 2) NOT NULL,
     store_id INT,
     date DATE NOT NULL, 
-    FOREIGN KEY (store_id) REFERENCES stores(store_id) ON DELETE CASCADE
+    FOREIGN KEY (store_id) REFERENCES stores(store_id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES products(product_id) ON DELETE CASCADE
 );
+CREATE INDEX idx_product_id ON grocery_items(product_id);
 
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -38,8 +47,15 @@ CREATE TABLE grocery_lists (
 CREATE TABLE grocery_list_items (
     grocery_list_item_id INT AUTO_INCREMENT PRIMARY KEY,
     grocery_list_id INT,
-    item_id INT,
+    product_id INT,
     quantity INT NOT NULL,
     FOREIGN KEY (grocery_list_id) REFERENCES grocery_lists(grocery_list_id) ON DELETE CASCADE,
-    FOREIGN KEY (item_id) REFERENCES grocery_items(item_id) ON DELETE CASCADE
+    FOREIGN KEY (product_id) REFERENCES grocery_items(product_id) ON DELETE CASCADE
+);
+
+create table blacklisted_tokens
+(
+    token                varchar(255) null,
+    blacklisted_token_id int          not null
+        primary key
 );
--- backend/src/main/resources/db/MockData.sql
+++ backend/src/main/resources/db/MockData.sql
@@ -1,11 +1,11 @@
 INSERT INTO users (first_name, last_name, email, password) VALUES
-('John', 'Doe', 'john.doe@example.com', 'password123'),
-('Jane', 'Smith', 'jane.smith@example.com', 'password456'),
-('Alice', 'Johnson', 'alice.johnson@example.com', 'password789'),
-('Bob', 'Brown', 'bob.brown@example.com', 'password101'),
-('Charlie', 'Davis', 'charlie.davis@example.com', 'password202'),
-('Eve', 'Miller', 'eve.miller@example.com', 'password303'),
-('Frank', 'Wilson', 'frank.wilson@example.com', 'password404');
+('John', 'Doe', 'john.doe@example.com', '$2a$10$7ktACsNjp/bLIwLVLVgPxuUiOwXvfU1FzS0Sfm.kX5pTJs/IWe5Sa'),
+('Jane', 'Smith', 'jane.smith@example.com', '$2a$10$7ktACsNjp/bLIwLVLVgPxuUiOwXvfU1FzS0Sfm.kX5pTJs/IWe5Sa'),
+('Alice', 'Johnson', 'alice.johnson@example.com', '$2a$10$7ktACsNjp/bLIwLVLVgPxuUiOwXvfU1FzS0Sfm.kX5pTJs/IWe5Sa'),
+('Bob', 'Brown', 'bob.brown@example.com', '$2a$10$7ktACsNjp/bLIwLVLVgPxuUiOwXvfU1FzS0Sfm.kX5pTJs/IWe5Sa'),
+('Charlie', 'Davis', 'charlie.davis@example.com', '$2a$10$7ktACsNjp/bLIwLVLVgPxuUiOwXvfU1FzS0Sfm.kX5pTJs/IWe5Sa'),
+('Eve', 'Miller', 'eve.miller@example.com', '$2a$10$7ktACsNjp/bLIwLVLVgPxuUiOwXvfU1FzS0Sfm.kX5pTJs/IWe5Sa'),
+('Frank', 'Wilson', 'frank.wilson@example.com', '$2a$10$7ktACsNjp/bLIwLVLVgPxuUiOwXvfU1FzS0Sfm.kX5pTJs/IWe5Sa');
 
 INSERT INTO grocery_lists (user_id, name, date) VALUES
 (1, 'Weekly Groceries', '2025-03-01'),
@@ -25,55 +25,64 @@ INSERT INTO stores (name, address) VALUES
 ('Metro', '303 Metro Dr, Montreal, QC'),
 ('Superstore', '404 Superstore St, Winnipeg, MB');
 
-INSERT INTO grocery_items (name, price, store_id, date) VALUES
-('Apple', 0.99, 1, '2025-03-01'),
-('Apple', 1.09, 2, '2025-03-02'),
-('Apple', 0.95, 3, '2025-03-03'),
-('Apple', 1.15, 4, '2025-03-04'),
-('Banana', 0.59, 1, '2025-03-01'),
-('Banana', 0.65, 2, '2025-03-02'),
-('Banana', 0.55, 3, '2025-03-03'),
-('Banana', 0.60, 4, '2025-03-04'),
-('Milk', 2.49, 1, '2025-03-01'),
-('Milk', 2.59, 2, '2025-03-02'),
-('Milk', 2.39, 3, '2025-03-03'),
-('Milk', 2.55, 4, '2025-03-04'),
-('Bread', 1.99, 1, '2025-03-01'),
-('Bread', 2.09, 2, '2025-03-02'),
-('Bread', 1.95, 3, '2025-03-03'),
-('Bread', 2.10, 4, '2025-03-04'),
-('Eggs', 3.49, 1, '2025-03-01'),
-('Eggs', 3.59, 2, '2025-03-02'),
-('Eggs', 3.39, 3, '2025-03-03'),
-('Eggs', 3.55, 4, '2025-03-04'),
-('Chicken Breast', 5.99, 1, '2025-03-01'),
-('Chicken Breast', 6.09, 2, '2025-03-02'),
-('Chicken Breast', 5.95, 3, '2025-03-03'),
-('Chicken Breast', 6.10, 4, '2025-03-04'),
-('Orange Juice', 3.99, 1, '2025-03-01'),
-('Orange Juice', 4.09, 2, '2025-03-02'),
-('Orange Juice', 3.95, 3, '2025-03-03'),
-('Orange Juice', 4.10, 4, '2025-03-04');
+INSERT INTO products (product_id, name) VALUES
+(1, 'Apple'),
+(2, 'Banana'),
+(3, 'Milk'),
+(4, 'Bread'),
+(5, 'Eggs'),
+(6, 'Chicken Breast'),
+(7, 'Orange Juice');
 
-INSERT INTO grocery_list_items (grocery_list_id, item_id, quantity) VALUES
+INSERT INTO grocery_items (product_id, price, store_id, date) VALUES
+(1, 0.99, 1, '2025-03-01'),
+(1, 1.09, 2, '2025-03-02'),
+(1, 0.95, 3, '2025-03-03'),
+(1, 1.15, 4, '2025-03-04'),
+(2, 0.59, 1, '2025-03-01'),
+(2, 0.65, 2, '2025-03-02'),
+(2, 0.55, 3, '2025-03-03'),
+(2, 0.60, 4, '2025-03-04'),
+(3, 2.49, 1, '2025-03-01'),
+(3, 2.59, 2, '2025-03-02'),
+(3, 2.39, 3, '2025-03-03'),
+(3, 2.55, 4, '2025-03-04'),
+(4, 1.99, 1, '2025-03-01'),
+(4, 2.09, 2, '2025-03-02'),
+(4, 1.95, 3, '2025-03-03'),
+(4, 2.10, 4, '2025-03-04'),
+(5, 3.49, 1, '2025-03-01'),
+(5, 3.59, 2, '2025-03-02'),
+(5, 3.39, 3, '2025-03-03'),
+(5, 3.55, 4, '2025-03-04'),
+(6, 5.99, 1, '2025-03-01'),
+(6, 6.09, 2, '2025-03-02'),
+(6, 5.95, 3, '2025-03-03'),
+(6, 6.10, 4, '2025-03-04'),
+(7, 3.99, 1, '2025-03-01'),
+(7, 4.09, 2, '2025-03-02'),
+(7, 3.95, 3, '2025-03-03'),
+(7, 4.10, 4, '2025-03-04');
+
+INSERT INTO grocery_list_items (grocery_list_id, product_id, quantity) VALUES
 (1, 1, 5),  -- Weekly Groceries, Apple from Walmart
-(1, 5, 10), -- Weekly Groceries, Banana from Walmart
-(1, 9, 2),  -- Weekly Groceries, Milk from Walmart
-(2, 13, 4), -- Party Supplies, Bread from T&T
-(2, 17, 3), -- Party Supplies, Eggs from T&T
-(2, 21, 6), -- Party Supplies, Chicken Breast from T&T
-(3, 25, 8), -- Monthly Stock, Orange Juice from T&T
-(3, 2, 7),  -- Monthly Stock, Apple from T&T
-(3, 6, 12), -- Monthly Stock, Banana from T&T
-(4, 10, 3), -- Vegetarian Week, Milk from NoFrills
-(4, 14, 5), -- Vegetarian Week, Bread from NoFrills
-(4, 18, 2), -- Vegetarian Week, Eggs from NoFrills
-(5, 22, 4), -- BBQ Essentials, Chicken Breast from NoFrills
-(5, 26, 6), -- BBQ Essentials, Orange Juice from NoFrills
-(5, 3, 9),  -- BBQ Essentials, Apple from NoFrills
-(6, 7, 11), -- Holiday Feast, Banana from NoFrills
-(6, 11, 2), -- Holiday Feast, Milk from Loblaws
-(6, 15, 4), -- Holiday Feast, Bread from Loblaws
-(7, 19, 3), -- Healthy Eating, Eggs from Loblaws
-(7, 23, 5), -- Healthy Eating, Chicken Breast from Loblaws
-(7, 27, 7); -- Healthy Eating, Orange Juice from Loblaws
+(1, 2, 10), -- Weekly Groceries, Banana from Walmart
+(1, 3, 2),  -- Weekly Groceries, Milk from Walmart
+(2, 4, 4), -- Party Supplies, Bread from T&T
+(2, 5, 3), -- Party Supplies, Eggs from T&T
+(2, 6, 6), -- Party Supplies, Chicken Breast from T&T
+(3, 7, 8), -- Monthly Stock, Orange Juice from T&T
+(3, 1, 7),  -- Monthly Stock, Apple from T&T
+(3, 2, 12), -- Monthly Stock, Banana from T&T
+(4, 3, 3), -- Vegetarian Week, Milk from NoFrills
+(4, 4, 5), -- Vegetarian Week, Bread from NoFrills
+(4, 5, 2), -- Vegetarian Week, Eggs from NoFrills
+(5, 6, 4), -- BBQ Essentials, Chicken Breast from NoFrills
+(5, 7, 6), -- BBQ Essentials, Orange Juice from NoFrills
+(5, 1, 9),  -- BBQ Essentials, Apple from NoFrills
+(6, 2, 11), -- Holiday Feast, Banana from NoFrills
+(6, 3, 2), -- Holiday Feast, Milk from Loblaws
+(6, 4, 4), -- Holiday Feast, Bread from Loblaws
+(7, 5, 3), -- Healthy Eating, Eggs from Loblaws
+(7, 6, 5), -- Healthy Eating, Chicken Breast from Loblaws
+(7, 7, 7); -- Healthy Eating, Orange Juice from Loblaws
