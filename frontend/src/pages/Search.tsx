42fc024d3a44d65fbb3eb4b2aa5ed6053bcc7b7b
arushlow <arushlow@uwo.ca>
2025-03-13 14:03:26 +0800

    Removed unnecessary comments and variables

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,96 +1,7 @@
-import { jwtDecode } from "jwt-decode";
 import React, { useState } from 'react';
 import Navbar from '../components/Navbar';
 import { useAuth } from "../hooks/useAuth";
 import '../style/Search.css';
-
-// type SearchProp = {};
-// type GroceryItem = {
-//   id: number;
-//   name: string;
-// };
-
-// const Search: React.FC<SearchProp> = () => {
-//   const [keyword, setKeyword] = useState('');
-//   const [filter, setFilter] = useState('item');
-//   const [found, setFound] = useState<GroceryItem[]>([]);
-
-//   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     setKeyword(e.target.value);
-//   };
-//   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-//     e.preventDefault();
-//     console.log('Searching for:', keyword);
-//     // Add your Search logic here
-//     const fetchItems = async () => {
-//       try {
-//         //filter by item
-//         if (filter == "item") {
-//           const response = await fetch(`http://localhost:8080/grocery-items/${keyword}`);
-
-//           if (!response.ok) {
-//             throw new Error("Failed to fetch grocery items.");
-//           }
-//           const items: GroceryItem[] = await response.json();
-//           setFound(items);
-//         }
-//         //filter by store
-//         else {
-//           const response = await fetch(`http://localhost:8080/grocery-items/${keyword}`);
-
-//           if (!response.ok) {
-//             throw new Error("Failed to fetch grocery items.");
-//           }
-//           const items: GroceryItem[] = await response.json();
-//           setFound(items);
-//         }
-//       } catch (error) {
-//         console.error("Error fetching items:", error);
-//         alert("Could not fetch items. Please try again later.");
-//       }
-
-//       fetchItems();
-//       console.log(found)
-//     }
-//   };
-//   const handleFilter = (e: React.ChangeEvent<HTMLSelectElement>) => {
-//     setFilter(e.target.value);
-//   };
-
-//   return (
-//     <>
-//       <Navbar />
-//       <div className="search">
-//         <h2>{"Search Page"}</h2>
-//         <form className="search-form" onSubmit={handleSubmit}>
-//           <div className="form-group">
-//             <label htmlFor="keyword">Keyword</label>
-//             <input
-//               type="text"
-//               id="keyword"
-//               name="keyword"
-//               onChange={handleChange}
-//               placeholder="Search..."
-//               required
-//             />
-//           </div>
-
-//           <div className="form-group">
-//             <label htmlFor="filter">Filter By:</label>
-//             <select id="filter" onChange={handleFilter}>
-//               <option value="item">Item Name</option>
-//               <option value="store">Store</option>
-//               <option value="test">Test</option>
-//             </select>
-//           </div>
-//           <button type="submit">Search</button>
-//         </form>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Search;
 
 type SearchProp = {};
 type GroceryItem = {
@@ -104,19 +15,13 @@ type Store = {
   name: string;
   address: string;
 };
-type Product = {
-  id: number;
-  name: string;
-};
 
 const Search: React.FC<SearchProp> = () => {
   const { token } = useAuth();
-  const userId = jwtDecode(token ? token : "").iss;
   const baseUrl = process.env.REACT_APP_BASE_URL;
   const [keyword, setKeyword] = useState('');
   const [filter, setFilter] = useState<GroceryItem[]>([]);
   const [found, setFound] = useState<GroceryItem[]>([]);
-  const [stores, setStores] = useState([]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setKeyword(e.target.value);
@@ -124,26 +29,9 @@ const Search: React.FC<SearchProp> = () => {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log('Searching for:', keyword);
-    // Add your Search logic here
+
     const fetchItems = async () => {
       try {
-        // template to pull from backend
-        //const response = await fetch(`http://localhost:8080/grocery-items/${keyword}`);
-
-        // const response = await fetch(`${baseUrl}/products/${keyword}`, {
-        //   method: "GET",
-        //   headers: {
-        //     Authorization: `Bearer ${token}`,
-        //   },
-        // });
-
-        // if (!response.ok) {
-        //   throw new Error("Failed to fetch grocery items.");
-        // }
-        // const groceries: Product[] = await response.json();
-        //console.dir(groceries, {'maxArrayLength': null});
-        //console.log(groceries[0].id)
-
         const response = await fetch(`${baseUrl}/search/${keyword}`, {
           method: "GET",
           headers: {
@@ -157,37 +45,7 @@ const Search: React.FC<SearchProp> = () => {
         const items: GroceryItem[] = await response.json();
 
         console.dir(items, {'maxArrayLength': null});
-        // const items: GroceryItem[] = [
-        //   {"product_id": 1, "price": 0.99, "store_id": 1, "date": "2025-03-01"},
-        //   {"product_id": 1, "price": 1.09, "store_id": 2, "date": "2025-03-02"},
-        //   {"product_id": 1, "price": 0.95, "store_id": 3, "date": "2025-03-03"},
-        //   {"product_id": 1, "price": 1.15, "store_id": 4, "date": "2025-03-04"}
-          // {"product_id": 2, "price": 0.59, "store_id": 1, "date": "2025-03-01"},
-          // {"product_id": 2, "price": 0.65, "store_id": 2, "date": "2025-03-02"},
-          // {"product_id": 2, "price": 0.55, "store_id": 3, "date": "2025-03-03"},
-          // {"product_id": 2, "price": 0.60, "store_id": 4, "date": "2025-03-04"},
-          // {"product_id": 3, "price": 2.49, "store_id": 1, "date": "2025-03-01"},
-          // {"product_id": 3, "price": 2.59, "store_id": 2, "date": "2025-03-02"},
-          // {"product_id": 3, "price": 2.39, "store_id": 3, "date": "2025-03-03"},
-          // {"product_id": 3, "price": 2.55, "store_id": 4, "date": "2025-03-04"},
-          // {"product_id": 4, "price": 1.99, "store_id": 1, "date": "2025-03-01"},
-          // {"product_id": 4, "price": 2.09, "store_id": 2, "date": "2025-03-02"},
-          // {"product_id": 4, "price": 1.95, "store_id": 3, "date": "2025-03-03"},
-          // {"product_id": 4, "price": 2.10, "store_id": 4, "date": "2025-03-04"},
-          // {"product_id": 5, "price": 3.49, "store_id": 1, "date": "2025-03-01"},
-          // {"product_id": 5, "price": 3.59, "store_id": 2, "date": "2025-03-02"},
-          // {"product_id": 5, "price": 3.39, "store_id": 3, "date": "2025-03-03"},
-          // {"product_id": 5, "price": 3.55, "store_id": 4, "date": "2025-03-04"},
-          // {"product_id": 6, "price": 5.99, "store_id": 1, "date": "2025-03-01"},
-          // {"product_id": 6, "price": 6.09, "store_id": 2, "date": "2025-03-02"},
-          // {"product_id": 6, "price": 5.95, "store_id": 3, "date": "2025-03-03"},
-          // {"product_id": 6, "price": 6.10, "store_id": 4, "date": "2025-03-04"},
-          // {"product_id": 7, "price": 3.99, "store_id": 1, "date": "2025-03-01"},
-          // {"product_id": 7, "price": 4.09, "store_id": 2, "date": "2025-03-02"},
-          // {"product_id": 7, "price": 3.95, "store_id": 3, "date": "2025-03-03"},
-          // {"product_id": 7, "price": 4.10, "store_id": 4, "date": "2025-03-04"}
-      //]
-      
+
         setFilter(items)
         setFound(items);
       } catch (error) {
