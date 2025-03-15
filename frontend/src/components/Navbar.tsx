3d328f97cf1be3667c47dc686f40bc956b92db93
arushlow <arushlow@uwo.ca>
2025-03-15 20:37:31 +0800

    Moved search to protected route in navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -17,10 +17,6 @@ const Navbar: React.FC = () => {
           <li>
             <RouterLink to="/">Home</RouterLink>
           </li>
-          <li>
-            <RouterLink to="/search">Search</RouterLink>
-          </li>
-
 
           {!token && (
             <>
@@ -35,6 +31,9 @@ const Navbar: React.FC = () => {
 
           {token && (
             <>
+              <li>
+                <RouterLink to="/search">Search</RouterLink>
+              </li>
               <li>
                 <RouterLink to="/shoppinglist">Create Grocery List</RouterLink>
               </li>
